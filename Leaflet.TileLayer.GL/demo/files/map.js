b4808521aee07bc019fb9a27c83edeee793a64a3
chezulkhairi <chezulkhairi.abdullah@gmail.com>
2018-08-19 16:15:23 +0100

    Update map.js

--- Leaflet.TileLayer.GL/demo/files/map.js
+++ Leaflet.TileLayer.GL/demo/files/map.js
@@ -4,6 +4,40 @@
  *
  * Author: https://www.openstreetmap.org/user/Zartbitter
  */
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min.js"></script>
+  <script src="https://api.tiles.mapbox.com/mapbox.js/v2.2.1/mapbox.js"></script>
+  <script src="voronoi_map.js"></script>
+	
+<script type="text/javascript">
+var fragmentShader = `
+void main(void) {
+	highp vec4 texelColour = texture2D(uTexture0, vec2(vTextureCoords.s, vTextureCoords.t));
+
+	// Height is represented in TENTHS of a meter
+	highp float height = (
+		texelColour.r * 255.0 * 256.0 * 256.0 +
+		texelColour.g * 255.0 * 256.0 +
+		texelColour.b * 255.0 )
+	-100000.0;
+
+	if (height > 100.0) {
+		// High (>30m) over ground, transparent
+		gl_FragColor = vec4(1.0, 0.5, 0.5, 0.0);
+	} else if (height > 50.0) {
+		// Over ground but somewhat close to sea level, yellow
+		gl_FragColor = vec4(0.9, 0.9, 0.5, 0.9);
+	} else if (height > 0.0) {
+		// Over ground but very close to sea level, red
+		gl_FragColor = vec4(0.9, 0.5, 1.0, 0.9);
+	} else {
+		// Water, some semiopaque blue
+		gl_FragColor = vec4(0.05, 0.1, 0.9, 0.9);
+	}
+}
+`
+</script>
+
+
 
 var map;
 
@@ -354,3 +388,23 @@ function initMap() {
 		navigator.geolocation.getCurrentPosition(foundLocation);
 	}
 }
+
+<script>
+    L.mapbox.accessToken = 'pk.eyJ1IjoiemV0dGVyIiwiYSI6ImVvQ3FGVlEifQ.jGp_PWb6xineYqezpSd7wA';
+	//var tileSize = 256;
+    var map = L.mapbox.map('map', 'zetter.i73ka9hn')
+      .fitBounds([[59.355596 , -9.052734], [49.894634 , 3.515625]]);
+    url = 'supermarkets.csv';
+    initialSelection = d3.set(['Tesco', 'Sainsburys']);
+    voronoiMap(map, url, initialSelection);
+	  
+	  var mapboxAccessToken = 'pk.eyJ1IjoienN0YWRsZXIiLCJhIjoiY2lvbDc5Zzl6MDAwc3Z2bTZ6NDNybDM3dSJ9.vxR1WVn26mEtpEk9MzdiUA';
+
+		var base = L.tileLayer("https://{s}.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken, {
+			attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy;<a href="https://www.mapbox.com/map-feedback/">Mapbox</a>'
+		}).addTo(map);
+	  
+	var antitoner = L.tileLayer.gl({
+			fragmentShader: fragmentShader,
+			tileUrls: ['https://{s}.tiles.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + mapboxAccessToken]
+		}).addTo(map);
