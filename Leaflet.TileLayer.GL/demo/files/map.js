245cc242db7e0b3d07e8175afe11994b19d422dc
chezulkhairi <chezulkhairi.abdullah@gmail.com>
2018-08-20 12:04:14 +0100

    Update map.js

--- Leaflet.TileLayer.GL/demo/files/map.js
+++ Leaflet.TileLayer.GL/demo/files/map.js
@@ -533,8 +533,8 @@ function initMap() {
 
 	var useGeolocation = true;
 	var zoom = 6;
-	var lat = 101.44724;
-	var lon = 2.7465;
+	var lat = 2.7465;
+	var lon = 101.44724;
 	var urlParams = getUrlParameters();
 	if (typeof urlParams.zoom != "undefined" && typeof urlParams.lat != "undefined" && typeof urlParams.lon != "undefined") {
 		zoom = urlParams.zoom;
