afa43f8c71003324b1bbafef8fc5fbc321afe0d4
chezulkhairi <chezulkhairi.abdullah@gmail.com>
2018-08-22 08:57:04 +0100

    Update toronto.js

--- Leaflet.TileLayer.GL/demo/toronto.js
+++ Leaflet.TileLayer.GL/demo/toronto.js
@@ -1,3 +1,34 @@
+var fragmentShader = `
+void main(void) {
+	highp vec4 texelColour = texture2D(uTexture0, vec2(vTextureCoords.s, vTextureCoords.t));
+	// Height is represented in TENTHS of a meter
+	highp float height = (
+		texelColour.r * 255.0 * 256.0 * 256.0 +
+		texelColour.g * 255.0 * 256.0 +
+		texelColour.b * 255.0 )
+	-100000.0;
+	if (height > 100.0) {
+		// High (>30m) over ground, transparent
+		gl_FragColor = vec4(0.5, 0.5, 0.5, 0.0);
+	} else if (height > 50.0) {
+		// Over ground but somewhat close to sea level, yellow
+		gl_FragColor = vec4(0.9, 0.9, 0.5, 0.4);
+	} else if (height > 0.0) {
+		// Over ground but very close to sea level, red
+		gl_FragColor = vec4(0.9, 0.5, 0.5, 0.4);
+	} else {
+		// Water, some semiopaque blue
+		gl_FragColor = vec4(0.05, 0.1, 0.9, 0.4);
+	}
+}
+`
+		var tileSize = 256;
+
+
+
+
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibXdpZGVuZXIiLCJhIjoibXBKQU85dyJ9.Q6yf1zk7wpnYqpsQfRwVmw';
 
 var map = new mapboxgl.Map({
@@ -83,6 +114,19 @@ var all_added_layers = []
 layers.forEach(function(layer,i){
   all_added_layers.push('building_layer-'+i);
 })
+
+		var antitoner = L.tileLayer.gl({
+			fragmentShader: fragmentShader,
+			tileUrls: ['https://{s}.tiles.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + mapboxAccessToken]
+		}).addTo(map);
+		function logEvent(e) { console.log(e.type); }
+		map.on('mousemove', function(ev){
+			document.getElementById('x').innerHTML = ev.latlng.lng;
+			document.getElementById('y').innerHTML = ev.latlng.lat;
+		});
+		map.on('zoomend', function(ev){
+			document.getElementById('zoom').innerHTML = map.getZoom();
+		});
 
 map.on('mousemove', function(e){
    features = map.queryRenderedFeatures(e.point, {all_added_layers}); //get features that are in building layers
