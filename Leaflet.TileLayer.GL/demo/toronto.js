a59b6e8349b44528cbb9e595915d76eb363ce25b
chezulkhairi <chezulkhairi.abdullah@gmail.com>
2018-08-22 09:34:07 +0100

    Update toronto.js

--- Leaflet.TileLayer.GL/demo/toronto.js
+++ Leaflet.TileLayer.GL/demo/toronto.js
@@ -1,28 +1,3 @@
-var fragmentShader = `
-void main(void) {
-	highp vec4 texelColour = texture2D(uTexture0, vec2(vTextureCoords.s, vTextureCoords.t));
-	// Height is represented in TENTHS of a meter
-	highp float height = (
-		texelColour.r * 255.0 * 256.0 * 256.0 +
-		texelColour.g * 255.0 * 256.0 +
-		texelColour.b * 255.0 )
-	-100000.0;
-	if (height > 100.0) {
-		// High (>30m) over ground, transparent
-		gl_FragColor = vec4(0.5, 0.5, 0.5, 0.0);
-	} else if (height > 50.0) {
-		// Over ground but somewhat close to sea level, yellow
-		gl_FragColor = vec4(0.9, 0.9, 0.5, 0.4);
-	} else if (height > 0.0) {
-		// Over ground but very close to sea level, red
-		gl_FragColor = vec4(0.9, 0.5, 0.5, 0.4);
-	} else {
-		// Water, some semiopaque blue
-		gl_FragColor = vec4(0.05, 0.1, 0.9, 0.4);
-	}
-}
-`
-
 mapboxgl.accessToken = 'pk.eyJ1IjoibXdpZGVuZXIiLCJhIjoibXBKQU85dyJ9.Q6yf1zk7wpnYqpsQfRwVmw';
 
 var map = new mapboxgl.Map({
@@ -35,15 +10,6 @@ var map = new mapboxgl.Map({
   pitch: 60,      //tilt camera
   bearing: 17.5   //adjust angle we're looking (17.5 degrees from north)
 });
-
-var mapboxAccessToken = 'pk.eyJ1IjoienN0YWRsZXIiLCJhIjoiY2lvbDc5Zzl6MDAwc3Z2bTZ6NDNybDM3dSJ9.vxR1WVn26mEtpEk9MzdiUA';
-var base = L.tileLayer("https://{s}.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken, {
-	attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy;<a href="https://www.mapbox.com/map-feedback/">Mapbox</a>'
-	}).addTo(map);
-var antitoner = L.tileLayer.gl({
-	fragmentShader: fragmentShader,
-	tileUrls: ['https://{s}.tiles.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + mapboxAccessToken]
-	}).addTo(map);
 
 var features=[];
 
@@ -68,8 +34,6 @@ map.on('style.load', function(){
     'type': 'vector',
     'url': 'mapbox://mwidener.1ayxbus1'
   });
-	
-
 
   layers.forEach(function(layer, i){
     map.addLayer({
@@ -119,9 +83,6 @@ var all_added_layers = []
 layers.forEach(function(layer,i){
   all_added_layers.push('building_layer-'+i);
 })
-
-		
-
 
 map.on('mousemove', function(e){
    features = map.queryRenderedFeatures(e.point, {all_added_layers}); //get features that are in building layers
