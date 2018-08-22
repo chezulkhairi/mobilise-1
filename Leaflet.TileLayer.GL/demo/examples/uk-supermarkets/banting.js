ef6aa20821a102f61ff8ae52a8e0b10a0eb8696a
chezulkhairi <chezulkhairi.abdullah@gmail.com>
2018-08-22 21:44:59 +0100

    Update banting.js

--- Leaflet.TileLayer.GL/demo/examples/uk-supermarkets/banting.js
+++ Leaflet.TileLayer.GL/demo/examples/uk-supermarkets/banting.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXdpZGVuZXIiLCJhIjoibXBKQU85dyJ9.Q6yf1zk7wpnYqpsQfRwVmw';
-
-var map = new mapboxgl.Map({
+var map;
+map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mwidener/ciuvowcno00ee2js5wibckxv3',
   center: [-79.388924,43.645],
@@ -77,6 +77,108 @@ map.on('style.load', function(){
                                                                                           //get the correct row, and add the appropriate text                                                                     //get the correct row, and add the appropriate text
   });
 });
+
+function loadGeoJsonString(geoString) {
+        var geojson = JSON.parse(geoString);
+        map.data.addGeoJson(geojson);
+        zoom(map);
+      }
+
+      /**
+       * Update a map's viewport to fit each geometry in a dataset
+       * @param {google.maps.Map} map The map to adjust
+       */
+      function zoom(map) {
+        var bounds = new google.maps.LatLngBounds();
+        map.data.forEach(function(feature) {
+          processPoints(feature.getGeometry(), bounds.extend, bounds);
+        });
+        map.fitBounds(bounds);
+      }
+
+      /**
+       * Process each point in a Geometry, regardless of how deep the points may lie.
+       * @param {google.maps.Data.Geometry} geometry The structure to process
+       * @param {function(google.maps.LatLng)} callback A function to call on each
+       *     LatLng point encountered (e.g. Array.push)
+       * @param {Object} thisArg The value of 'this' as provided to 'callback' (e.g.
+       *     myArray)
+       */
+      function processPoints(geometry, callback, thisArg) {
+        if (geometry instanceof google.maps.LatLng) {
+          callback.call(thisArg, geometry);
+        } else if (geometry instanceof google.maps.Data.Point) {
+          callback.call(thisArg, geometry.get());
+        } else {
+          geometry.getArray().forEach(function(g) {
+            processPoints(g, callback, thisArg);
+          });
+        }
+      }
+
+
+      /* DOM (drag/drop) functions */
+
+      function initEvents() {
+        // set up the drag & drop events
+        var mapContainer = document.getElementById('map');
+        var dropContainer = document.getElementById('drop-container');
+
+        // map-specific events
+        mapContainer.addEventListener('dragenter', showPanel, false);
+
+        // overlay specific events (since it only appears once drag starts)
+        dropContainer.addEventListener('dragover', showPanel, false);
+        dropContainer.addEventListener('drop', handleDrop, false);
+        dropContainer.addEventListener('dragleave', hidePanel, false);
+      }
+
+      function showPanel(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        document.getElementById('drop-container').style.display = 'block';
+        return false;
+      }
+
+      function hidePanel(e) {
+        document.getElementById('drop-container').style.display = 'none';
+      }
+
+      function handleDrop(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        hidePanel(e);
+
+        var files = e.dataTransfer.files;
+        if (files.length) {
+          // process file(s) being dropped
+          // grab the file data from each file
+          for (var i = 0, file; file = files[i]; i++) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+              loadGeoJsonString(e.target.result);
+            };
+            reader.onerror = function(e) {
+              console.error('reading failed');
+            };
+            reader.readAsText(file);
+          }
+        } else {
+          // process non-file (e.g. text or html) content being dropped
+          // grab the plain text version of the data
+          var plainText = e.dataTransfer.getData('text/plain');
+          if (plainText) {
+            loadGeoJsonString(plainText);
+          }
+        }
+
+        // prevent drag event from bubbling further
+        return false;
+      }
+
+
+
+
 
 //create an array that has the name of all layers created above
 var all_added_layers = []
